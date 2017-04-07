c46b1a4e3c3971eb3b399d4505e6276f492b0527
tomfriwel <976932447@qq.com>
2017-04-07 16:56:28 +0800

    Initial Commit

---
+++ GetArticle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetArticle
+//
+//  Created by tomfriwel on 07/04/2017.
+//  Copyright © 2017 tomfriwel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GetArticle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetArticle
+//
+//  Created by tomfriwel on 07/04/2017.
+//  Copyright © 2017 tomfriwel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
