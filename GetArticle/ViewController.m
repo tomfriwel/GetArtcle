79638fb2697852d9d153c1c258996539e10b0f04
tomfriwel <976932447@qq.com>
2017-04-08 22:58:57 +0800

    add html file

--- GetArticle/ViewController.m
+++ GetArticle/ViewController.m
@@ -8,8 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIWebViewDelegate>
 
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
 @end
 
 @implementation ViewController
@@ -17,12 +18,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+//    NSURL *url = [NSURL URLWithString:@"http://u.jue.so/p/folio.php?GAL=a276d98d740a5e11c7cfe5b66e77"];
+//    NSString *webData= [NSString stringWithContentsOfURL:url];
+//    NSLog(@"%@",webData);
+    
+    [self loadWeb];
 }
 
+-(void)loadWeb{
+    NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"PocketContent" ofType:@"html" inDirectory:@"PocketHTML"]];
+    [self.webView loadRequest:[NSURLRequest requestWithURL:url]];
+}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - UIWebViewDelegate
+
+-(void)webViewDidFinishLoad:(UIWebView *)webView {
+    
 }
 
 
